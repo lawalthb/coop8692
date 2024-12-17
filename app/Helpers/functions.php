c3e09a1a604ed854f1c6b40d011c97f72c715059
lawalthb <38714153+lawalthb@users.noreply.github.com>
2024-12-17 13:15:38 +0100

    current balance 2

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -1,12 +1,13 @@
 <?php
-
+$currentBalance = 0;
 function generateReference($prefix = '')
 {
     return $prefix . time() . rand(1000, 9999);
 }
 
-    return $currentBalance + $newAmount;
-function calculateBalance($transactions) {
+return $currentBalance + $newAmount;
+function calculateBalance($transactions)
+{
     $currentBalance = 0; // Initialize balance
 
     foreach ($transactions as $transaction) {
